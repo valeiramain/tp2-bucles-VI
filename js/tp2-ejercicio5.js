17acf4b2d5c061622a52ab2fa0320b553219da7b
Valentina Iramain <valentinairamain2020@gmail.com>
2025-04-18 19:37:41 -0300

    continuando ejercicio 5. falta mensaje mostrando resultado

--- js/tp2-ejercicio5.js
+++ js/tp2-ejercicio5.js
@@ -14,83 +14,98 @@
 // Output: ‘L’
 // Dificultad:  🟢🟡
 
-const dni = parseInt(prompt("Ingrese el DNI (sin puntos): "));
-if (dni>=0 && dni<=99999999){
-    const resto = dni%23;
-    switch (resto){
-        case 0:
-            letra = "T"
-            break;
-        case 1:
-            letra = "R"
-            break;
-        case 2:
-            letra = "W"
-            break;
+let dni = 0;
+let letra = "";
+do {
+    dni = parseInt(prompt("Ingrese el DNI (sin puntos): "));
+    if (dni >= 0 && dni <= 99999999) {
+        const resto = dni % 23;
+        console.log("dni" + dni)
+        console.log("resto" + resto)
+        switch (resto) {
+            case 0:
+                letra = "T"
+                break;
+            case 1:
+                letra = "R"
+                break;
+            case 2:
+                letra = "W"
+                break;
+            case 3:
+                letra = "A"
+                break;
+            case 4:
+                letra = "G"
+                break;
+            case 5:
+                letra = "M"
+                break;
+            case 6:
+                letra = "Y"
+                break;
+            case 7:
+                letra = "F"
+                break;
+            case 8:
+                letra = "P"
+                break;
+            case 9:
+                letra = "D"
+                break;
+            case 10:
+                letra = "X"
+                break;
+            case 11:
+                letra = "B"
+                break;
+            case 12:
+                letra = "N"
+                break;
+            case 13:
+                letra = "J"
+                break;
+            case 14:
+                letra = "Z"
+                break;
+            case 15:
+                letra = "S"
+                break;
+            case 16:
+                letra = "Q"
+                break;
+            case 17:
+                letra = "V"
+                break;
+            case 18:
+                letra = "H"
+                break;
+            case 19:
+                letra = "L"
+                break;
+            case 20:
+                letra = "C"
+                break;
+            case 21:
+                letra = "K"
+                break;
+            case 22:
+                letra = "E"
+                break;
+            default:
+                letra="";
+                document.writeln("operación inválida")
+        }
+        console.log("letra " + letra)
 
-        case 3:
-            letra = "A"
-            break;
-            
-        case 4:
-            letra = "G"
-            break;
-        case 5:
-            letra = "M"
-            break;
-        case 6:
-            letra = "Y"
-            break;
-        case 7:
-            letra = "F"
-            break;
-        case 8:
-            letra = "P"
-            break;
-        case 9:
-            letra = "D"
-            break;
-        case 10:
-            letra = "X"
-            break;
-        case 11:
-            letra = "B"
-            break;
-        case 12:
-            letra = "N"
-            break;
-        case 13:
-            letra = "J"
-            break;
-        case 14:
-            letra = "Z"
-            break;
-        case 15:
-            letra = "S"
-            break;
-        case 16:
-            letra = "Q"
-            break;
-        case 17:
-            letra = "V"
-            break;
-        case 18:
-            letra = "H"
-            break;
-        case 19:
-            letra = "L"
-            break;
-        case 20:
-            letra = "C"
-            break;
-        case 21:
-            letra = "K"
-            break;
-        case 22:
-            letra = "E"
-            break;
-        default:
+        if (letra !== "") {
+            document.writeln("Al dni " + dni + " le corresponde la letra " + letra+"<br>")
+            document.writeln(`Al dni ${dni} le corresponde la letra ${letra+} <br>`)
+        }
+    } else {
+        alert("DNI incorrecto")
     }
-}else{
-    document.writeln("DNI incorrecto")
-}
+
+} while (confirm("¿Desea continuar cargando DNI?"));
+
+
