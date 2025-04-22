bc013efc9268d0108c71dac49d4aaba1f1f46293
Valentina Iramain <valentinairamain2020@gmail.com>
2025-04-22 18:00:03 -0300

    se realiza ejercicio 10 explicado en clase el dia 22/4

--- js/tp2-ejercicio10.js
+++ js/tp2-ejercicio10.js
@@ -1,3 +1,27 @@
 //10- Realiza un script que pida número de filas y columnas y escriba una tabla.
-//  Dentro de cada una de las celdas deberá escribirse un número consecutivo en orden descendente.
-// Si, por ejemplo, la tabla es de 7×5 los números irán del 35 al 1.
+//  Dentro de cada una de las celdas deberá escribirse un número consecutivo en
+//  orden descendente. Si, por ejemplo, la tabla es de 7×5 los números irán del 35 al 1.
+// <tr> son filas
+// <td> son columnas
+
+const filas = parseInt(prompt("Ingrese número de filas: "))
+const columnas = parseInt(prompt("Ingrese número de columnas: "))
+let contenido = filas * columnas
+
+document.writeln(` <table>
+        <tbody>`)
+
+for (let indiceFilas = 1; indiceFilas <= filas; indiceFilas++) {
+    document.writeln(`<tr>`)
+    for (let indiceColumnas = 1; indiceColumnas <= columnas; indiceColumnas++) {
+        document.writeln(`<td>${contenido}</td>`)
+        contenido = contenido - 1
+    }
+    document.writeln(`</tr>`)
+}
+
+document.writeln(`
+        </tbody>
+    </table>`)
+
+
